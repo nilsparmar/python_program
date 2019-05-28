aac60c175e400b57c4fd0c967d8c4181ff5179da
Wilson Parmar <Wilsonchristian39@gmail.com>
2019-05-28 10:21:20 +0530

    count info, warning & error func

---
+++ count.py
@@ -0,0 +1,48 @@
+
+import re
+
+''' This is simple read function with take file name with full file path and returns content of file
+     ex. read_file('/tmp/a.txt')'''
+
+
+def read_file(file_path):
+
+	with open(file_path, 'r') as read_file:
+
+		file_cont = read_file.read()
+
+	return file_cont
+
+'''Simple regex to find INFO, ERROR, WARRING from files'''
+
+info = re.compile(r'INFO')
+warn = re.compile(r'WARNING')
+error = re.compile(r'ERROR')
+
+#Just to verify 
+#data = read_file('/home/wilson/Downloads/log.txt')
+#all_result = warn.findall(data)
+
+
+
+'''' This is simple function which return count of INFO, WARNNING and ERROR from a file'''
+
+def count_no(file):
+
+	file_data = read_file(file)
+
+	info_count = info.findall(file_data)
+	warn_count = warn.findall(file_data)
+	error_count = error.findall(file_data)
+
+	info_count = len(info_count)
+	warn_count = len(warn_count)
+	error_count = len(error_count)
+
+	return(info_count, warn_count, error_count)
+
+file = 'D:/Python/nils/python_program/log.txt'	
+
+info, warn, err = count_no(file)
+
+print(info, warn, err)
