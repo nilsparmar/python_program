21956da93ef48276097346c4636963c8097b39fb
Wilson Parmar <Wilsonchristian39@gmail.com>
2019-05-29 09:47:46 +0530

    New task hide Number

---
+++ hide_no.py
@@ -0,0 +1,4 @@
+## Create a funtion which takes a No (12 digit)
+## Hide first 8 digit with "*" same as our bank acclunt & return
+## No should be 12 digit long
+## Add any other functionality if want to add
